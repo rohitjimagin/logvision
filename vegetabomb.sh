1ffbf93db9c6ef5b2beb053fd6b85859f3504650
rohit <rohit.j@imaginnovate.com>
2023-02-26 12:51:21 +0530

    updations

--- vegetabomb.sh
+++ vegetabomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Create 3 products
-for i in {1..200}
+for i in {1..20000}
 do
     curl -s -X POST -H "Content-Type: application/json" \
         -d '{"product": {"name": "Product '$i'", "price": "'$i'00", "brand": "Brand '$i'"}}' \
         http://localhost:80/api/v1/products | jq -r '.id' >> product_ids.txt
+    sleep 5 # Add a 5-second delay between each request
 done
 
 # Get product IDs from file
@@ -20,12 +20,15 @@ for (( i=0; i<${#product_ids[@]}; i++ )); do
     curl -s -X PUT -H "Content-Type: application/json" \
       -d '{"product": {"name": "Updated product", "price": "999", "brand": "Updated brand"}}' \
       http://localhost:80/api/v1/products/$product_id
+    sleep 5 # Add a 5-second delay between each request
   # Delete the other products
   else
     product_id=${product_ids[$i]}
     echo "Deleting product $product_id"
     curl -s -X DELETE http://localhost:80/api/v1/products/$product_id
+    sleep 5 # Add a 5-second delay between each request
   fi
 done
+
 # Clean up
 rm product_ids.txt
